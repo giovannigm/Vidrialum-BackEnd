828b803d4047c10fd2e5c9e91c70d748302027b1
giovannigm <giovanniantonygarcia@gmail.com>
2025-02-12 00:46:49 -0300

    Add multer for file uploads in emailController

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,32 +1,54 @@
 const nodemailer = require("nodemailer");
 const config = require("../config/config");
+const multer = require("multer");
+
+// Configurar Multer correctamente
+const upload = multer({ storage: multer.memoryStorage() });
 
 exports.sendEmail = (req, res) => {
-  const { user_name, user_email, message } = req.body;
-  console.log("Datos recibidos:", req.body);
-
-  const transporter = nodemailer.createTransport({
-    service: config.emailService,
-    auth: {
-      user: config.emailUser,
-      pass: config.emailPass,
-    },
-  });
-
-  const mailOptions = {
-    from: config.emailUser,
-    to: config.emailUser,
-    subject: "PAGINA WEB VIDRIALUM",
-    text: `Nombre: ${user_name}\nCorreo: ${user_email}\nDescripción: ${message}`,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.status(500).send("Error al enviar el correo electrónico");
-    } else {
-      console.log("Correo electrónico enviado:", info.response);
-      res.status(200).send("Correo electrónico enviado con éxito");
+  upload.single("file")(req, res, (err) => {
+    if (err) {
+      return res.status(500).send("Error al subir el archivo");
     }
+
+    const { nombre, email, message } = req.body;
+    console.log("Datos recibidos:", req.body);
+    console.log("Archivo recibido:", req.file);
+
+    // Configurar Nodemailer
+    const transporter = nodemailer.createTransport({
+      service: config.emailService,
+      auth: {
+        user: config.emailUser,
+        pass: config.emailPass,
+      },
+    });
+
+    // Configurar opciones del correo
+    const mailOptions = {
+      from: config.emailUser,
+      to: config.emailUser,
+      subject: "PAGINA WEB VIDRIALUM - Nuevo Mensaje",
+      text: `Nombre: ${nombre}\nCorreo: ${email}\nMensaje: ${message}`,
+      attachments: req.file
+        ? [
+            {
+              filename: req.file.originalname,
+              content: req.file.buffer,
+            },
+          ]
+        : [],
+    };
+
+    // Enviar el correo
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        res.status(500).send("Error al enviar el correo electrónico");
+      } else {
+        console.log("Correo electrónico enviado:", info.response);
+        res.status(200).send("Correo enviado con éxito");
+      }
+    });
   });
 };
